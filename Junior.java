1f4a3da5b6ed4fd669d17842b5614aee7b6d3758
Sprawl888 <62518948+Sprawl888@users.noreply.github.com>
2020-03-23 17:03:48 +0300

    Add files via upload

--- Junior.java
+++ Junior.java
@@ -11,28 +11,17 @@ import java.util.concurrent.TimeUnit;
 
 public class Junior {
     @Test
-    public void junior() {
+    public void junior() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\spraw\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.get("https://www.google.com/");
         String titlegoogle = driver.getTitle();
         // Проверка перехода на страницу Google
         Assert.assertTrue(titlegoogle.equals("Google"));
         WebElement YO = driver.findElement(By.name("q"));
-        //String href = driver.getCurrentUrl();
-        //Assert.assertFalse(href.equals("https://market.yandex.ru/"));
-        //WebElement R = driver.findElement(By.linkText("Яндекс.Маркет — выбор и покупка товаров из ..."));
-        //R.getCssValue();
-        //System.out.println("abc abc abc"+R.getTagName());
-        //String href = driver.getCurrentUrl();
-        //Assert.assertFalse(href.equals("https://market.yandex.ru/"));
         YO.sendKeys("Яндекс Маркет");
         YO.submit();
-        //String href = driver.getCurrentUrl();
-        //Assert.assertFalse(href.equals("https://market.yandex.ru/"));
-        /**String OP = driver.getTitle();
-        Assert.assertFalse(OP.equals("Яндекс.Маркет — выбор и покупка товаров из проверенных интернет-магазинов"));*/
         WebElement L = driver.findElement(By.xpath("//h3[contains(text(), 'Яндекс.Маркет — выбор и покупка товаров из ...')]"));
         // Проверка отображения ссылки на странице поиска
         Assert.assertTrue(L.isDisplayed());
@@ -43,31 +32,44 @@ public class Junior {
         WebElement OI = driver.findElement(By.id("header-search"));
         OI.sendKeys("Пылесосы");
         OI.submit();
+        TimeUnit.SECONDS.sleep(4);
         String pylisos = driver.getTitle();
-        // Проверка перехода на страницу раздела "Пылесоса"
+        // Проверка перехода на страницу раздела "Пылесосов"
         Assert.assertTrue(pylisos.equals("«Пылесосы» — Результаты поиска — Яндекс.Маркет"));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         WebElement N = driver.findElement(By.className("Q2TzG3eu2W"));
         N.click();
         WebElement U = driver.findElement(By.xpath("//button[contains(text(), 'Показать всё')]"));
+        //Assert.assertTrue(U.isDisplayed());
         U.click();
+        TimeUnit.SECONDS.sleep(4);
+        WebElement ONYX = driver.findElement(By.className("_1JYTt02WxW"));
+        ONYX.sendKeys("Polaris");
+        ONYX.click();
+        TimeUnit.SECONDS.sleep(4);
+        WebElement Z = driver.findElement(By.xpath("//span[contains(text(), 'Polaris')]"));
+        Z.click();
+        TimeUnit.SECONDS.sleep(4);
+        WebElement FOX = driver.findElement(By.className("_19zvwl8AGi"));
+        FOX.click();
+        TimeUnit.SECONDS.sleep(4);
+        ONYX.sendKeys("Vitek");
+        ONYX.click();
+        TimeUnit.SECONDS.sleep(4);
+        WebElement G = driver.findElement(By.xpath("//span[contains(text(), 'VITEK')]"));
+        G.click();
+        TimeUnit.SECONDS.sleep(4);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         WebElement X = driver.findElement(By.xpath("//span[contains(text(), 'В продаже')]"));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         X.click();
+        TimeUnit.SECONDS.sleep(4);
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        WebElement Z = driver.findElement(By.xpath("//span[contains(text(), 'Polaris')]"));
-        Assert.assertTrue(Z.isDisplayed());
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        Z.click();
-        WebElement G = driver.findElement(By.xpath("//span[contains(text(), 'VITEK')]"));
-        Assert.assertTrue(G.isDisplayed());
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        G.click();
         WebElement Q = driver.findElement(By.id("glpriceto"));
         Q.sendKeys("6000");
-        Q.submit();
-
+        Q.click();
+        WebElement OPS = driver.findElement(By.xpath("//span[contains(text(), 'Найдено 36 результатов')]"));
+        //Проверка количества результатов фильтрации
+        Assert.assertTrue(OPS.getText().equals("Найдено 36 результатов"));
+        driver.quit();
         }
     }
 
